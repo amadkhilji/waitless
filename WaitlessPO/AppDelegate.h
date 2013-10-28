6283c070c491c106e47e62338e1610679743c5bb
Amad Khilji <amad_khilji@hotmail.com>
2013-10-28 22:41:35 +0500

    Initial Commit

---
+++ WaitlessPO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WaitlessPO
+//
+//  Created by Amad Khilji on 28/10/2013.
+//  Copyright (c) 2013 Amad Khilji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WaitlessPO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WaitlessPO
+//
+//  Created by Amad Khilji on 28/10/2013.
+//  Copyright (c) 2013 Amad Khilji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
